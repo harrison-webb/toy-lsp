aea9988289911bb3c77d3946b01421b2d97c6109
Harrison Webb <harrison.dev.00@gmail.com>
2024-07-13 01:21:18 -0600

    hover!!!

---
+++ lsp/initialize.go
@@ -0,0 +1,55 @@
+package lsp
+
+type InitializeRequest struct {
+	Request
+	Params InitializeRequestParams `json:"params"`
+}
+
+type InitializeRequestParams struct {
+	ClientInfo *ClientInfo `json:"clientInfo"`
+	// lots more that goes here
+}
+
+type ClientInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type InitializeResponse struct {
+	Response
+	Result InitializeResult `json:"result"`
+}
+
+type InitializeResult struct {
+	Capabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo   ServerInfo         `json:"serverInfo"`
+}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type ServerCapabilities struct {
+	TextDocumentSync int  `json:"textDocumentSync"`
+	HoverProvider    bool `json:"hoverProvider"`
+}
+
+func NewInitializeResponse(id int) InitializeResponse {
+	return InitializeResponse{
+		Response: Response{
+			RPC: "2.0",
+			ID:  &id,
+		},
+		Result: InitializeResult{
+			Capabilities: ServerCapabilities{
+				TextDocumentSync: 1, // 1 means send the full content of the document
+				HoverProvider:    true,
+			},
+			ServerInfo: ServerInfo{
+				Name:    "toylsp",
+				Version: "0.1",
+			},
+		},
+	}
+}
--- main.go
+++ main.go
@@ -2,21 +2,109 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"io"
+	"log"
 	"os"
+	"toylsp/analysis"
+	"toylsp/lsp"
 	"toylsp/rpc"
 )
 
 func main() {
-
+	logger := getLogger("/home/harrison/Laboratory/toy-lsp/log.txt")
+	logger.Println("Logger started")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 
+	state := analysis.NewState()
+	writer := os.Stdout
+
 	for scanner.Scan() {
-		msg := scanner.Text()
-		handleMessage(msg)
+		msg := scanner.Bytes()
+		method, contents, err := rpc.DecodeMessage(msg)
+		if err != nil {
+			logger.Printf("Got an error: %s", err)
+			continue
+		}
+
+		handleMessage(logger, writer, state, method, contents)
 	}
 }
 
-func handleMessage(msg any) {
+func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, method string, contents []byte) {
+	logger.Printf("Received message with method: %s", method)
+
+	switch method {
+	case "initialize":
+		var request lsp.InitializeRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("Could not parse initialize request: %s", err)
+		}
+
+		logger.Printf("Connected to: %s %s", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+
+		// lets reply
+		msg := lsp.NewInitializeResponse(request.ID)
+		writeResponse(writer, msg)
+
+		logger.Print("Sent the reply")
+
+	case "textDocument/didOpen": // TODO: not getting this message from neovim, need to read the docs to see if im missing something
+		var request lsp.DidOpenTextDocumentNotification
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("Could not parse textDocument/didOpen %s", err)
+			return
+		}
+
+		logger.Printf("Opened: %s", request.Params.TextDocument.URI)
+		state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+	case "textDocument/didChange":
+		var request lsp.DidChangeTextDocumentNotification
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("Could not parse textDocument/didChange: %s", err)
+			return
+		}
+
+		logger.Printf("Changed: %s", request.Params.TextDocument.URI)
+		for _, change := range request.Params.ContentChanges {
+			state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+		}
+	case "textDocument/hover":
+		var request lsp.HoverRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("Could not parse textDocument/hover: %s", err)
+			return
+		}
+
+		// create a response and write it back
+		response := lsp.HoverResponse{
+			Response: lsp.Response{
+				RPC: "2.0",
+				ID:  &request.ID,
+			},
+			Result: lsp.HoverResult{
+				Contents: "Hello from LSP",
+			},
+		}
+
+		// write the response back
+		writeResponse(writer, response)
+
+	}
+}
+
+func writeResponse(writer io.Writer, msg any) {
+	reply := rpc.EncodeMessage(msg)
+	writer.Write([]byte(reply))
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("Invalid file provided")
+	}
+
+	return log.New(logfile, "[toy-lsp]", log.Ldate|log.Ltime|log.Lshortfile)
 
 }
